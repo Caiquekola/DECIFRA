0bb3ebc1aea20f3c64a4560522b7146a5a01495b
Caique  Augusto <caiquekola@hotmail.com>
2025-08-29 16:16:09 -0300

    UI:

--- DECIFRA/decifra/src/pages/header/header.tsx
+++ DECIFRA/decifra/src/pages/header/header.tsx
@@ -1,45 +1,48 @@
 import './header.css';
 
 function Header(): React.ReactElement {
-  return (
-    <header>
-      <div className="left-icons">
-        <div className="icon">
-          {/* Ícone 1 */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-            fill="currentColor" className="bi bi-arrow-down-square" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
-          </svg>
-        </div>
-        <div className="icon">
-          {/* Ícone 2 */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26"
-            fill="currentColor" className="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
-            <path d="M11 2a1 1 0 0 1 1-1h2..."/>
-          </svg>
-        </div>
-      </div>''
+    return (
+        <header>
+            <div className="left-icons">
+                <div className="icon">
+                    {/* Ícone 1 */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        fill="currentColor" className="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                    </svg>
+                </div>
+                <div className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" 
+                        className="bi bi-question-square" viewBox="0 0 16 16">
+                        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                        <path d="M5.255 5.786a.237.237 0 0 0 .241.247h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286m1.557 5.763c0 .533.425.927 1.01.927.609 0 1.028-.394 1.028-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94" />
+                    </svg>
+                </div>
+            </div>
 
-      <h1>DECIFRA</h1>
+            <h1>DECIFRA</h1>
 
-      <div className="right-icons">
-        <div className="icon">
-          {/* Ícone engrenagem 1 */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-            fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
-            <path d="M8 4.754a3.246 3.246 0..."/>
-          </svg>
-        </div>
-        <div className="icon">
-          {/* Ícone engrenagem 2 */}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-            fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
-            <path d="M8 4.754a3.246 3.246 0..."/>
-          </svg>
-        </div>
-      </div>
-    </header>
-  )
+            <div className="right-icons">
+                <div className="icon">
+                    {/* Ícone 3 */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="26"
+                        fill="currentColor" className="bi bi-bar-chart-line-fill" viewBox="0 0 16 16">
+                        <path d="M11 2a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v12h.5a.5.5 0 0 1 0 1H.5a.5.5 0 0 1 0-1H1v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h1V7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7h1z" />
+                    </svg>
+                    {/* Ícone 4 Engrenagem */}
+                </div>
+                <div className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        fill="currentColor" className="bi bi-gear" viewBox="0 0 16 16">
+                        <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492M5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0" />
+                        <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115z" />
+
+                    </svg>
+                </div>
+
+            </div>
+        </header>
+    )
 }
 
 export default Header;
