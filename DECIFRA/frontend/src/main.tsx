c50d056a233c3223dd32d690ff8b7deb24650c31
Caique Augusto <caique.augusto@unsoft.com.br>
2025-12-23 18:03:48 -0300

    init sad

--- DECIFRA/frontend/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM  from 'react-dom/client';
-import Home from "./pages/index";
-import "./styles/index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
-root.render(<Home />);
-
--- DECIFRA/frontend/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    root: './src', // Define a pasta raiz como "src"
-    build: {
-        outDir: '../dist' // Saída para a pasta "dist"
-    }
-});
